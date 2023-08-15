f688304c072f5ff6e7c0984355845374cf05ea60
DarkCat09 <darkcat09@vivaldi.net>
2023-08-15 14:57:10 +0400

    Fixed HTML formatting; added description

--- src/routes/start.ts
+++ src/routes/start.ts
@@ -1,8 +1,13 @@
 import { FastifyInstance } from "fastify";
 import { engineList } from "../handlers/main";
 
+const desc = (
+  "txtdot is a HTTP proxy that parses text, links and pictures " +
+  "from pages reducing internet traffic, removing ads and heavy scripts"
+);
+
 export default async function parseRoute(fastify: FastifyInstance) {
   fastify.get("/start", async (_, reply) => {
-    return reply.view("/templates/start.ejs", { engineList });
+    return reply.view("/templates/start.ejs", { desc, engineList });
   });
 }
