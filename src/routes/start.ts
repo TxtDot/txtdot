4dbc4fc8a0fa3cc2338c8ab72cc51ef21653cd6e
DarkCat09 <darkcat09@vivaldi.net>
2023-08-15 15:55:18 +0400

    Basic HTML + CSS; description moved to template

--- src/routes/start.ts
+++ src/routes/start.ts
@@ -1,13 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { engineList } from "../handlers/main";
 
-const desc = (
-  "txtdot is a HTTP proxy that parses text, links and pictures " +
-  "from pages reducing internet traffic, removing ads and heavy scripts"
-);
-
 export default async function parseRoute(fastify: FastifyInstance) {
   fastify.get("/start", async (_, reply) => {
-    return reply.view("/templates/start.ejs", { desc, engineList });
+    return reply.view("/templates/start.ejs", { engineList });
   });
 }
