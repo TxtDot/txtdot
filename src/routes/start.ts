23a6484e20960071aa8c2871dcf543256cccd8af
Artemy <artegoser@gmail.com>
2023-08-15 13:11:09 +0300

    feat: dynamic engine list in start page

--- src/routes/start.ts
+++ src/routes/start.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance } from "fastify";
+import { engineList } from "../handlers/main";
 
 export default async function parseRoute(fastify: FastifyInstance) {
   fastify.get("/start", async (_, reply) => {
-    return reply.view("/templates/start.ejs");
+    return reply.view("/templates/start.ejs", { engineList });
   });
 }
