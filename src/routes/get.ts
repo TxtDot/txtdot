20755f2b8986cc27ddc68f95f3f91722033d36aa
Artemy <artegoser@gmail.com>
2023-08-16 11:54:22 +0300

    doc: schema for get

--- src/routes/get.ts
+++ src/routes/get.ts
@@ -1,38 +1,42 @@
 import { FastifyInstance } from "fastify";
 
-import { GetRequest } from "../types/requests";
+import { GetSchema, IGetSchema } from "../types/requests";
 import handlePage from "../handlers/main";
 import { generateRequestUrl } from "../utils";
 
 export default async function getRoute(fastify: FastifyInstance) {
-  fastify.get("/get", async (request: GetRequest, reply) => {
-    const remoteUrl = request.query.url;
-    const engine = request.query.engine;
+  fastify.get<IGetSchema>(
+    "/get",
+    { schema: GetSchema },
+    async (request, reply) => {
+      const remoteUrl = request.query.url;
+      const engine = request.query.engine;
 
-    let format: string;
+      let format: string;
 
-    if (request.query.format === "text") {
-      reply.type("text/plain; charset=utf-8");
-      format = "text";
-    } else {
-      reply.type("text/html; charset=utf-8");
-      format = "html";
+      if (request.query.format === "text") {
+        reply.type("text/plain; charset=utf-8");
+        format = "text";
+      } else {
+        reply.type("text/html; charset=utf-8");
+        format = "html";
+      }
+
+      const parsed = await handlePage(
+        remoteUrl,
+        generateRequestUrl(
+          request.protocol,
+          request.hostname,
+          request.originalUrl
+        ),
+        engine
+      );
+
+      if (format === "text") {
+        return parsed.textContent;
+      } else {
+        return reply.view("/templates/get.ejs", { parsed: parsed });
+      }
     }
-
-    const parsed = await handlePage(
-      remoteUrl,
-      generateRequestUrl(
-        request.protocol,
-        request.hostname,
-        request.originalUrl
-      ),
-      engine
-    );
-
-    if (format === "text") {
-      return parsed.textContent;
-    } else {
-      return reply.view("/templates/get.ejs", { parsed: parsed });
-    }
-  });
+  );
 }
--- src/types/requests.ts
+++ src/types/requests.ts
@@ -1,4 +1,4 @@
-import { FastifyRequest } from "fastify";
+import { FastifyRequest, FastifySchema } from "fastify";
 
 export type GetRequest = FastifyRequest<{
   Querystring: {
@@ -7,6 +7,43 @@ export type GetRequest = FastifyRequest<{
     engine?: string;
   };
 }>;
+
+export interface IGetQuery {
+  url: string;
+  format?: string;
+  engine?: string;
+}
+
+export interface IGetSchema {
+  Querystring: IGetQuery;
+}
+
+export const getQuerySchema = {
+  type: "object",
+  required: ["url"],
+  properties: {
+    url: {
+      type: "string",
+      description: "URL",
+    },
+    format: {
+      type: "string",
+      enum: ["text", "html", ""],
+      default: "html",
+    },
+    engine: {
+      type: "string",
+      enum: ["readability", "google", ""],
+      default: "readability",
+    },
+  },
+};
+
+export const GetSchema: FastifySchema = {
+  description: "Get page",
+  querystring: getQuerySchema,
+  produces: ["text/html"],
+};
 
 export type EngineRequest = FastifyRequest<{
   Querystring: {
