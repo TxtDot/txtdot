08a89190ed64457c004535dabf642407a83a238c
Artemy <artegoser@gmail.com>
2023-08-18 11:38:05 +0300

    fix: proxy url

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -3,6 +3,7 @@ import { config } from "dotenv";
 export class ConfigService {
   public readonly host: string;
   public readonly port: number;
+  public readonly reverse_proxy_enabled: boolean;
 
   constructor() {
     const parsed = config().parsed;
@@ -11,7 +12,9 @@ export class ConfigService {
       throw new Error("Invalid .env file");
     }
 
-    this.host = process.env.HOST || 'localhost';
+    this.host = process.env.HOST || "localhost";
     this.port = Number(process.env.PORT) || 8080;
+    this.reverse_proxy_enabled =
+      Boolean(process.env.REVERSE_PROXY_ENABLED) || false;
   }
 }
