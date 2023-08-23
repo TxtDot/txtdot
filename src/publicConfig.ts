c2ed53d3bbc472f53fe20cccba65460105aa8280
Artemy Egorov <artegoser@gmail.com>
2023-08-23 17:24:17 +0300

    doc: api version fix

--- src/publicConfig.ts
+++ src/publicConfig.ts
@@ -1,5 +1,5 @@
 export default {
-  version: "1.1.0",
+  version: "1.1.1",
   description:
     "HTTP proxy that parses only text, links and pictures from pages reducing internet traffic, removing ads and heavy scripts",
 };
