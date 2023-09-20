9f0fd5f7a8be1fe7592d659ce6c35261fbd104ce
DarkCat09 <gh@dc09.ru>
2023-09-20 14:54:51 +0400

    Using version and description from publicConfig; v1.3.1

--- src/routes/browser/index.ts
+++ src/routes/browser/index.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from "fastify";
+
+import publicConfig from "../../publicConfig";
 import { engineList } from "../../handlers/main";
 import { indexSchema } from "../../types/requests/browser";
 
 export default async function indexRoute(fastify: FastifyInstance) {
   fastify.get("/", { schema: indexSchema }, async (_, reply) => {
-    return reply.view("/templates/index.ejs", { engineList });
+    return reply.view("/templates/index.ejs", { publicConfig, engineList });
   });
 }
