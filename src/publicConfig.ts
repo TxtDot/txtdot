afdfa42036681f31d58bfaafe2e9a87b23dc8233
Artemy <artegoser@gmail.com>
2024-01-06 20:42:54 +0300

    chore: update version

--- src/publicConfig.ts
+++ src/publicConfig.ts
@@ -1,5 +1,5 @@
 export default {
-  version: '1.4.0',
+  version: '1.5.0',
   description:
     'txtdot is an HTTP proxy that parses only text, links and pictures from pages reducing internet bandwidth usage, removing ads and heavy scripts',
 };
