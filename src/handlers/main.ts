2c53c5a67c081905e36a11ec268dcb616de47658
Artemy <artegoser@gmail.com>
2024-02-17 12:39:30 +0300

    delete: google engine
    useless since searx added

--- src/handlers/main.ts
+++ src/handlers/main.ts
@@ -6,29 +6,5 @@ const distributor = new Distributor();
 distributor.engine(Readability);
 distributor.engine(SearX);
 
-// export const engines: Engines = {
-//   readability,
-//   google,
-//   stackoverflow,
-//   searx,
-// };
-
-// export const engineList: string[] = Object.keys(engines);
-
-// export const fallback: EnginesMatch = [
-//   {
-//     pattern: GoogleDomains,
-//     engine: engines.google,
-//   },
-//   {
-//     pattern: StackOverflowDomains,
-//     engine: engines.stackoverflow,
-//   },
-//   {
-//     pattern: SearxDomains,
-//     engine: engines.searx,
-//   },
-// ];
-
 export const engineList = distributor.list;
 export default distributor;
