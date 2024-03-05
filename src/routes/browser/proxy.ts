e70f78fb495de8c950abd4443876b8b962791bfd
Artemy <artegoser@gmail.com>
2024-03-05 21:51:17 +0300

    fix: isLocalResourse for proxy
     GHSA-4c78-229v-hf6m

--- src/routes/browser/proxy.ts
+++ src/routes/browser/proxy.ts
@@ -2,11 +2,18 @@ import { FastifyInstance } from 'fastify';
 import { IProxySchema, ProxySchema } from '../../types/requests/browser';
 import axios from '../../types/axios';
 
+import isLocalResource from '../../utils/islocal';
+import { LocalResourceError } from '../../errors/main';
+
 export default async function proxyRoute(fastify: FastifyInstance) {
   fastify.get<IProxySchema>(
     '/proxy',
     { schema: ProxySchema },
     async (request, reply) => {
+      if (await isLocalResource(new URL(request.query.url))) {
+        throw new LocalResourceError();
+      }
+
       const response = await axios.get(request.query.url);
       const mime: string | undefined =
         response.headers['content-type']?.toString();
