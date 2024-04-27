037f7b5de6b380d41728b4ac24508397b6117339
Artemy <artegoser@gmail.com>
2024-04-27 22:13:29 +0300

    fix: engine plugins default list

--- src/config/pluginConfig.ts
+++ src/config/pluginConfig.ts
@@ -1,12 +1,12 @@
 import { IAppConfig } from '../types/appConfig';
-import { engines } from '@txtdot/plugins';
+import { engineList } from '@txtdot/plugins';
 
 /**
  * Configuration of plugins
  * Here you can add your own plugins
  */
 const plugin_config: IAppConfig = {
-  engines: [...Object.values(engines)],
+  engines: [...engineList],
 };
 
 export default plugin_config;
