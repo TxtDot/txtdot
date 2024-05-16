8ca194cd380b35c5564dd32327b71f77549e9bd7
Artemy <artegoser@gmail.com>
2024-05-16 11:32:56 +0300

    feat: big badge configuration

--- packages/server/src/routes/browser/configuration.ts
+++ packages/server/src/routes/browser/configuration.ts
@@ -6,11 +6,28 @@ import { indexSchema } from '../../types/requests/browser';
 import config from '../../config';
 
 export default async function configurationRoute(fastify: FastifyInstance) {
-  fastify.get('/configuration', { schema: indexSchema }, async (_, reply) => {
-    return reply.view('/templates/configuration.ejs', {
-      engines: distributor.engines_fallback,
-      middlewares: distributor.middles_fallback,
-      config,
-    });
-  });
+  fastify.get(
+    '/configuration',
+    { schema: indexSchema },
+    async (request, reply) => {
+      return reply.view('/templates/configuration.ejs', {
+        engines: distributor.engines_fallback,
+        middlewares: distributor.middles_fallback,
+        config,
+        base_url: `(${request.protocol}) - ${request.headers.host}`,
+      });
+    }
+  );
+
+  fastify.get(
+    '/configuration/badges/big.svg',
+    { schema: indexSchema },
+    async (request, reply) => {
+      reply.header('content-type', 'image/svg+xml');
+      return reply.view('/templates/big_badge.ejs', {
+        config,
+        base_url: `(${request.protocol}) - ${request.headers.host}`,
+      });
+    }
+  );
 }
